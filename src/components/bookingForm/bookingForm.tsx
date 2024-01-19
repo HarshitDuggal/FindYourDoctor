993e647d7958323248ec01ead910d9c65b262535
Harshit Duggal <harshitduggal101@gmail.com>
2024-01-19 15:37:18 +0530

    dev: Create Booking Form

--- src/components/bookingForm/bookingForm.tsx
+++ src/components/bookingForm/bookingForm.tsx
@@ -1,8 +1,132 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
+import "./bookingFormStyles.css";
 
-const bookingForm = () => {
+const BookingForm = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    phoneNumber: "",
+    age: "",
+    city: "",
+    company: "",
+    chiefComplaints: "",
+    previousExperience: "",
+  });
+
+  const [step, setStep] = useState(1);
+  const [formFilled, setFormFilled] = useState(false);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleNextStep = () => {
+    if (formData.name && formData.phoneNumber) {
+      setStep((prevStep) => prevStep + 1);
+    }
+  };
+
+  const handleFormSubmit = () => {
+    setStep(5);
+  };
+
   return (
-    <div>bookingForm</div>
-  )
-}
+    <motion.div
+      className="form-box"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 4 }}
+    >
+      {step === 1 && (
+        <>
+          <input
+            required
+            type="text"
+            name="name"
+            placeholder="Name"
+            value={formData.name}
+            onChange={handleInputChange}
+          />
+          <input
+            required
+            type="tel"
+            name="phoneNumber"
+            placeholder="Phone Number"
+            value={formData.phoneNumber}
+            onChange={handleInputChange}
+          />
+          <button onClick={handleNextStep}>Next</button>
+        </>
+      )}
+      {step === 2 && (
+        <>
+          <input
+            required
+            type="text"
+            name="age"
+            placeholder="Age"
+            value={formData.age}
+            onChange={handleInputChange}
+          />
+          <input
+            required
+            type="text"
+            name="city"
+            placeholder="City"
+            value={formData.city}
+            onChange={handleInputChange}
+          />
+          <input
+            required
+            type="text"
+            name="company"
+            placeholder="Company"
+            value={formData.company}
+            onChange={handleInputChange}
+          />
+          <button onClick={handleNextStep}>Next</button>
+        </>
+      )}
+      {step === 3 && (
+        <>
+          <input
+            required
+            type="text"
+            name="chiefComplaints"
+            placeholder="Chief Complaints"
+            value={formData.chiefComplaints}
+            onChange={handleInputChange}
+          />
+          <button onClick={handleNextStep}>Next</button>
+        </>
+      )}
 
-export default bookingForm
+      {step === 4 && (
+        <>
+          <input
+            required
+            type="text"
+            name="previousExperience"
+            placeholder="Any previous experience with physiotherapy"
+            value={formData.previousExperience}
+            onChange={handleInputChange}
+          />
+          <button
+            onClick={handleFormSubmit}
+            disabled={!formData.previousExperience}
+          >
+            Submit
+          </button>
+        </>
+      )}
+      {step === 5 && (
+        <div style={{ color: "white" }}>
+          Recommended Doctors coming your way
+        </div>
+      )}
+    </motion.div>
+  );
+};
+
+export default BookingForm;
--- src/screens/home/homeScreen.tsx
+++ src/screens/home/homeScreen.tsx
@@ -1,23 +1,63 @@
-import Navbar from '../../components/navbar/navbar'
-import HeroImage from '../../assets/svg/HeroImage.svg'
-import './homeScreenStyles.css';
-import {motion} from 'framer-motion';
+import Navbar from "../../components/navbar/navbar";
+import HeroImage from "../../assets/svg/HeroImage.svg";
+import BookingForm from "../../components/bookingForm/bookingForm";
+import "./homeScreenStyles.css";
+import { motion } from "framer-motion";
 
+const heroImageVarient = {
+  hidden: {
+    opacity: 0,
+    x: "15vw",
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 3,
+    },
+  },
+};
+const logoImageVarient = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 2,
+    },
+  },
+};
 
 const Home = () => {
   return (
-    <div className='home-container'>
+    <div className="home-container">
       <div>
-      <Navbar/>
+        <Navbar />
       </div>
-      <div className='image-container'>
-        <img src='https://ik.imagekit.io/tcfp7i31d/logo_with_yp_black_urUeyjKwY.svg' alt='Logo'/>
+      <div className="image-container">
+        <motion.img
+          variants={logoImageVarient}
+          initial="hidden"
+          animate="visible"
+          src="https://ik.imagekit.io/tcfp7i31d/logo_with_yp_black_urUeyjKwY.svg"
+          alt="Logo"
+        />
       </div>
-      <div className='heroImage-container'>
-      <img src={HeroImage} alt='Logo'/>
+      <div className="heroImage-container">
+        <motion.img
+          variants={heroImageVarient}
+          initial="hidden"
+          animate="visible"
+          src={HeroImage}
+          alt="Logo"
+        />
+        <section>
+          <BookingForm />
+        </section>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
